69104478feb52bbbd83b5803c7cb7c18f0606fb0
vartikasharma <vartika@saavn.com>
2019-08-30 02:50:54 +0530

    added recyclerview for contacts

--- android/AndroidThreading/app/src/main/java/com/example/androidthreading/BaseActivity.java
+++ android/AndroidThreading/app/src/main/java/com/example/androidthreading/BaseActivity.java
@@ -1,14 +1,36 @@
 package com.example.androidthreading;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class BaseActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BaseActivity extends AppCompatActivity {
+    private static final String LOG_TAG = BaseActivity.class.getSimpleName();
+    private static final Integer listNum = 20;
+    private ArrayList<Contact> contacts;
+
+    @BindView(R.id.rv_list)
+    RecyclerView recyclerView;
+
+    private ContactsAdapter contactsAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        ButterKnife.bind(this);
+        contacts = Contact.createContactsList(listNum);
+        contactsAdapter = new ContactsAdapter(contacts);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
     }
 }
---
+++ android/AndroidThreading/app/src/main/java/com/example/androidthreading/Contact.java
@@ -0,0 +1,32 @@
+package com.example.androidthreading;
+
+import java.util.ArrayList;
+
+public class Contact {
+    private String name;
+    private boolean isOnline;
+
+    public Contact(String name, boolean isOnline) {
+        this.name = name;
+        this.isOnline = isOnline;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+    private static int lastContactId = 0;
+
+    public static ArrayList<Contact> createContactsList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new Contact("Person " + ++lastContactId, i <= numContacts / 2));
+        }
+
+        return contacts;
+    }
+}
