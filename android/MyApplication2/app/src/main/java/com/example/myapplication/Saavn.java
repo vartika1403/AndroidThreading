8f82930ee11d48353956b5d31cbb33f075aeb8fe
vartikasharma <vartika@saavn.com>
2019-09-05 19:06:29 +0530

    changed gradle version

---
+++ android/MyApplication2/app/src/main/java/com/example/myapplication/Saavn.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+import androidx.multidex.MultiDexApplication;
+
+public class Saavn  extends MultiDexApplication {
+    public void onCreate() {
+        super.onCreate();
+    }
+}
